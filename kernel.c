2f30090b245b4c3901c7553072270ed1d565cf45
Debra Nash <dnash@smu.edu>
2013-10-26 20:40:11 -0500

    Successfully displaying Hello on screen

--- kernel.c
+++ kernel.c
@@ -5,7 +5,11 @@
 
 int main()
 {
-	putInMemory(0xB000, 0x8140, 'A');
+	putInMemory(0xB000, 0x8140, 'H');
+	putInMemory(0xB000, 0x8142, 'e');
+	putInMemory(0xB000, 0x8144, 'l');
+	putInMemory(0xB000, 0x8146, 'l');
+	putInMemory(0xB000, 0x8148, 'o');
 	putInMemory(0xB000, 0x8141, 0x7);
 
 	while (1) {
